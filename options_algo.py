1c2c04bca6e970ff1106a40a5e967656fff902c7
Bryce Kunschick <bcool2bryce@gmail.com>
2025-06-08 19:23:05 -0400

    Testing API connection to Robinhood

--- options_algo.py
+++ options_algo.py
@@ -1,6 +1,10 @@
 import robin_stocks as rs
 import robin_stocks.robinhood as rh
 import os
+from dotenv import load_dotenv
+
+# Load env variables
+load_dotenv()
 
 username = os.getenv("RH_USERNAME")
 password = os.getenv("RH_PASSWORD")
@@ -10,6 +14,3 @@ login = rh.login(username, password)
 my_stocks = rh.build_holdings()
 
 print(my_stocks)
-
-print(username)
-print(password)
